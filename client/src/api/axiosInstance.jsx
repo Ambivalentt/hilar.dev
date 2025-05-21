830d98045e927be1f6a0a5067616517657d9bb3a
Ambivalentt <stackov3r@gmail.com>
2025-05-21 15:31:57 -0500

    type modules

--- client/src/api/axiosInstance.jsx
+++ client/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://hilar-backend.onrender.com'
+    baseURL: 'hilar-dev.railway.internal'
     // baseURL: 'http://localhost:3000'
 });
 
